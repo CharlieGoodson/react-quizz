ab9a88c6d70dee7de6ed2d0895ad571a243de85c
CharlieGoodson <max.sadchikov@gmail.com>
2018-11-03 17:21:12 +0200

    after 12

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 
 class Quiz extends Component {
 
     state = {
+        results: {},
+        isFinished: false,
         activeQuiz: 0,
         answerState: null,
         quiz: [
             {
+                id: 1,
                 question: 'Какого цвета небо?',
                 rightAnswerId: 3,
                 answers: [
@@ -19,7 +23,8 @@ class Quiz extends Component {
                 ]
             },
             {
-                question: 'Какая столица США?',
+                id: 2,
+                question: 'Где находится столица США?',
                 rightAnswerId: 4,
                 answers: [
                     {text: 'Вашингтон', id: 4},
@@ -38,14 +43,22 @@ class Quiz extends Component {
         }
 
         const currentQuestion = this.state.quiz[this.state.activeQuiz]
+        const results = this.state.results
 
         if (currentQuestion.rightAnswerId === answerId) {
-
-            this.setState({ answerState: { [answerId]: 'success'} })
+            if (!results[currentQuestion.id]) {
+                results[currentQuestion.id] = 'success'
+            }
+            this.setState({
+                answerState: { [answerId]: 'success'},
+                results
+            })
+            
             const timeout = setTimeout(() => {
-                
                 if (this.isQuizFinish()) {
-                    console.log('Finished!')
+                    this.setState({
+                        isFinished: true
+                    })
                 } else {
                     this.setState({
                         activeQuiz: this.state.activeQuiz + 1,
@@ -55,7 +68,10 @@ class Quiz extends Component {
                 clearTimeout(timeout)
             }, 1000)
         } else {
-            this.setState({ answerState: { [answerId]: 'error'} })
+            results[currentQuestion] = 'error'
+            this.setState({ answerState: { [answerId]: 'error'},
+            results   
+        })
         }
     }
 
@@ -63,19 +79,36 @@ class Quiz extends Component {
         return this.state.activeQuiz + 1 === this.state.quiz.length
     }
 
+    retryHandler = () =>
+        this.setState({
+            results: {},
+            isFinished: false,
+            activeQuiz: 0,
+            answerState: null
+        })
+
     render() {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
                     <h1>Ответьте на все вопросы</h1>
-                    <ActiveQuiz
-                        answers={this.state.quiz[this.state.activeQuiz].answers} 
-                        question={this.state.quiz[this.state.activeQuiz].question}
-                        onAnswerClick={this.onAnswerClickHandler}
-                        quizNumber={this.state.activeQuiz + 1}
-                        quizLength={this.state.quiz.length}
-                        state={this.state.answerState}
-                    />
+
+                    {
+                        this.state.isFinished
+                        ? <FinishedQuiz 
+                                results={this.state.results}
+                                quiz={this.state.quiz}
+                                onRetry={this.retryHandler}
+                            />
+                        : <ActiveQuiz
+                            answers={this.state.quiz[this.state.activeQuiz].answers} 
+                            question={this.state.quiz[this.state.activeQuiz].question}
+                            onAnswerClick={this.onAnswerClickHandler}
+                            quizNumber={this.state.activeQuiz + 1}
+                            quizLength={this.state.quiz.length}
+                            state={this.state.answerState}
+                        />
+                    }
                 </div>
             </div>
         )
